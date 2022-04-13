95541391196ec0418071b00b7e6433e23f96fdc8
Aprian Firlanda Imani <aprianfirlanda@gmail.com>
2022-04-13 15:55:42 +0700

    add SecurityConfig and endpoint /login

--- src/main/kotlin/com/aprianfirlanda/SpringSecurityKotlinApplication.kt
+++ src/main/kotlin/com/aprianfirlanda/SpringSecurityKotlinApplication.kt
@@ -1,10 +1,17 @@
 package com.aprianfirlanda
 
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import java.security.Principal
 
 @SpringBootApplication
@@ -15,10 +22,29 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class UserController {
+class UserController(
+	private val passwordEncoder: PasswordEncoder,
+	private val userService: ReactiveUserDetailsService
+) {
+
+	@PostMapping("/login")
+	suspend fun login(@RequestBody login: Login): Jwt {
+		val user = userService.findByUsername(login.username).awaitSingleOrNull()
+
+		user?.let {
+			if (passwordEncoder.matches(login.password, it.password)) {
+				return Jwt("sample")
+			}
+		}
+
+		throw ResponseStatusException(HttpStatus.UNAUTHORIZED)
+	}
 
 	@GetMapping("/me")
 	suspend fun me(@AuthenticationPrincipal principal: Principal): Profile = Profile(principal.name)
 }
+
+data class Login(val username: String, val password: String)
+data class Jwt(val token: String)
 
 data class Profile(val username: String)
