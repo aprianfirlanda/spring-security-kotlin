437c5d41a31e581ac3c3907f78e1fefd10fcec1e
Aprian Firlanda Imani <aprianfirlanda@gmail.com>
2022-04-13 14:24:51 +0700

    init project

---
+++ src/main/kotlin/com/aprianfirlanda/SpringSecurityKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.aprianfirlanda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/aprianfirlanda/SpringSecurityKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aprianfirlanda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
