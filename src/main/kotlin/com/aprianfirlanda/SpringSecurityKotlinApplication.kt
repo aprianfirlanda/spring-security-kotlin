7cbe2fe0ab904a7d3d5051996d32bcd996ab8984
Aprian Firlanda Imani <aprianfirlanda@gmail.com>
2022-04-13 14:45:15 +0700

    add endpoint GET /me which need authentication

--- src/main/kotlin/com/aprianfirlanda/SpringSecurityKotlinApplication.kt
+++ src/main/kotlin/com/aprianfirlanda/SpringSecurityKotlinApplication.kt
@@ -2,6 +2,10 @@ package com.aprianfirlanda
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.security.Principal
 
 @SpringBootApplication
 class SpringSecurityKotlinApplication
@@ -9,3 +13,12 @@ class SpringSecurityKotlinApplication
 fun main(args: Array<String>) {
 	runApplication<SpringSecurityKotlinApplication>(*args)
 }
+
+@RestController
+class UserController {
+
+	@GetMapping("/me")
+	suspend fun me(@AuthenticationPrincipal principal: Principal): Profile = Profile(principal.name)
+}
+
+data class Profile(val username: String)
